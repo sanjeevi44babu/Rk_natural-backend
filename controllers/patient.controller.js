31d96f2c93b20ad5f2e59ed57dba5ad3a4a90157
sanjeevi Babu <141345761+sanjeevi44babu@users.noreply.github.com>
2026-05-01 18:16:52 +0530

    Update patient.controller.js

--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import Patient from "../models/patient.model.js";
+import Patient from "../models/Patient.model.js";
 
 // ===============================
 // GET /patients
